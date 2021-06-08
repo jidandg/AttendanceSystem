34b164860bedc1ad476454cb6e587e091ad8e7b5
philipusrenaldy <philipusrenaldy@gmail.com>
2021-06-08 17:51:32 +0700

    Update

--- MD/AttendanceSystem/app/src/main/java/com/marwinjidopi/attendancesystem/data/UserForm.kt
+++ MD/AttendanceSystem/app/src/main/java/com/marwinjidopi/attendancesystem/data/UserForm.kt
@@ -3,7 +3,7 @@ package com.marwinjidopi.attendancesystem.data
 import android.graphics.Bitmap
 
 data class UserForm(
-    var img: Bitmap,
+    var img: String,
     var name: String,
     var nim: String,
     var semester: String,
