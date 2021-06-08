a645a9da0804b5ee6b06a1f7d4d254476e0b3ef5
ngeeeeng <jidandhirayogagumbira@gmail.com>
2021-06-08 18:04:57 +0700

    Set status bar and action bar on splash screen activity to be hidden

--- MD/AttendanceSystem/app/src/main/java/com/marwinjidopi/attendancesystem/ui/SplashScreenActivity.kt
+++ MD/AttendanceSystem/app/src/main/java/com/marwinjidopi/attendancesystem/ui/SplashScreenActivity.kt
@@ -1,9 +1,12 @@
 package com.marwinjidopi.attendancesystem.ui
 
 import android.content.Intent
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import android.view.WindowInsets
+import android.view.WindowManager
 import com.marwinjidopi.attendancesystem.R
 import com.marwinjidopi.attendancesystem.ui.registerlogin.LoginActivity
 
@@ -11,6 +14,15 @@ class SplashScreenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.insetsController?.hide(WindowInsets.Type.statusBars())
+        } else {
+            window.setFlags(
+                WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
+                WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS
+            )
+        }
 
         Handler(mainLooper).postDelayed({
             val intent = Intent(this, LoginActivity::class.java)
